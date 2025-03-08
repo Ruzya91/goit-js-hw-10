c505aece115e5ca0c7001d7f44af8486e7f9996e
Ruzya91 <pogosian.ruz@gmail.com>
2025-03-08 18:15:56 +0000

    Deploying to gh-pages from @ Ruzya91/goit-js-hw-10@4cd33cd33621e8629bc29dfe532d9c641fe4d860 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-D5A5_ACS.js";import{i}from"./assets/vendor-BbbuE1sJ.js";document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.elements.delay.value),o=t.target.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"✅ Success",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"❌ Error",message:`Rejected promise in ${e}ms`,position:"topRight"})}),t.target.reset()});
+//# sourceMappingURL=2-snackbar.js.map
